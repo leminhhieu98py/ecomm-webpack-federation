e4b5de6f3e3ff78b0d2bf9cf24e259db6fc8c793
leminhhieu98py <leminhhieu98py@gmail.com>
2024-09-14 22:15:56 +0700

    config for marketing deploymeny and connect container to marketing part on PROD

--- packages/container/config/webpack.prod.js
+++ packages/container/config/webpack.prod.js
@@ -7,14 +7,14 @@ const domain = process.env.PRODUCTION_DOMAIN;
 const prodConfig = {
   mode: 'production',
   output: {
-    filename: '[name][contenthash].js',
-    publicPath: '/container/latest'
+    filename: '[name].[contenthash].js',
+    publicPath: '/container/latest/'
   },
   plugins: [
     new ModuleFederationPlugin({
       name: 'container',
       remotes: {
-        marketing: `marketing@${domain}/marketing /remoteEntry.js`
+        marketing: `marketing@${domain}/marketing/latest/remoteEntry.js`
       },
       shared: ['react', 'react-dom']
     })
--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -4,7 +4,9 @@ import Marketing from './RemoteComponents/Marketing';
 export default () => {
   return (
     <>
-      <h1>Root container </h1>
+      <h1>Root container</h1>
+      {/* TODO: remove after testing AWS deployment */}
+      <h1>hello</h1>
       <hr />
       <Marketing />
     </>
