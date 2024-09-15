cbfe7e47d5c331f7c3bbc70d9f7f5896dcbd4b89
leminhhieu98py <leminhhieu98py@gmail.com>
2024-09-15 14:12:14 +0700

    fix style collision by adding prefix

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -2,12 +2,22 @@ import React from 'react';
 import Marketing from './RemoteComponents/Marketing';
 import Header from './components/Header';
 import { BrowserRouter } from 'react-router-dom';
+import {
+  StylesProvider,
+  createGenerateClassName
+} from '@material-ui/core/styles';
+
+const generateClassName = createGenerateClassName({
+  productionPrefix: 'container'
+});
 
 export default () => {
   return (
-    <BrowserRouter>
-      <Header />
-      <Marketing />
-    </BrowserRouter>
+    <StylesProvider generateClassName={generateClassName}>
+      <BrowserRouter>
+        <Header />
+        <Marketing />
+      </BrowserRouter>
+    </StylesProvider>
   );
 };
