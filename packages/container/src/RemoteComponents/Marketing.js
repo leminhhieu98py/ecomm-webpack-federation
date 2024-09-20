a9b3f93deedff81e2074270e13ca2295c6324f3a
leminhhieu98py <leminhhieu98py@gmail.com>
2024-09-20 17:10:36 +0700

    setting BrowserHistory for container and MemoryHistory for marketing

--- packages/container/src/RemoteComponents/Marketing.js
+++ packages/container/src/RemoteComponents/Marketing.js
@@ -2,12 +2,26 @@ import React from 'react';
 import { useEffect } from 'react';
 import { useRef } from 'react';
 import { mount } from 'marketing/MarketingApp';
+import { useHistory } from 'react-router-dom';
 
 const Marketing = () => {
   const marketingRef = useRef();
+  const history = useHistory();
+
+  const onMarketingNavigate = (marketingLocation) => {
+    const { pathname } = history.location;
+
+    if (marketingLocation !== pathname) {
+      history.push(marketingLocation);
+    }
+  };
 
   useEffect(() => {
-    mount(marketingRef.current);
+    const { onParentNavigate } = mount(marketingRef.current, {
+      onMarketingNavigate
+    });
+
+    history.listen(onParentNavigate);
   }, []);
 
   return <div ref={marketingRef}></div>;
--- packages/marketing/src/mount.js
+++ packages/marketing/src/mount.js
@@ -1,15 +1,37 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createMemoryHistory, createBrowserHistory } from 'history';
 import App from './App';
 
-const mount = (element) => {
-  ReactDOM.render(<App />, element);
+const mount = (element, { onMarketingNavigate, isMemoryHistory = true }) => {
+  const history = isMemoryHistory
+    ? createMemoryHistory()
+    : createBrowserHistory();
+
+  if (onMarketingNavigate) {
+    history.listen(onMarketingNavigate);
+  }
+
+  ReactDOM.render(<App history={history} />, element);
+
+  const onParentNavigate = (location) => {
+    const { pathname: marketingPathName } = history.location;
+    const parentPathname = location.pathname;
+
+    if (parentPathname !== marketingPathName) {
+      history.push(parentPathname);
+    }
+  };
+
+  return {
+    onParentNavigate
+  };
 };
 
 const container = document.querySelector('#_marketing-container');
 
 if (container) {
-  mount(container);
+  mount(container, { isMemoryHistory: false });
 }
 
 export { mount };
